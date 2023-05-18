d4f1cebe3599f14a1ad727126a3e308098ed4ce1
hizumi <nyatel@yandex.ru>
2023-05-18 12:44:49 +0400

    add exam1

---
+++ exam1/Program.cs
@@ -0,0 +1,22 @@
+﻿//методы и функции
+
+// void SayLang(string name){
+//     Console.WriteLine($"привет, {name}");
+// }
+// SayLang("python");
+// SayLang("c#");
+// string lang = "c++";
+// SayLang(lang);
+
+int SayMax(int num1, int num2){
+    if(num1 > num2) return num1;
+    else return num2;
+}
+//один из вариантов вывода
+Console.WriteLine(SayMax(3, 5));
+//второй вариант вывода
+int num = SayMax(4, 7);
+Console.WriteLine(num);
+//и еще один вариант
+int x = 9, y = 6, num1 = SayMax(x, y);
+Console.WriteLine(num1);
